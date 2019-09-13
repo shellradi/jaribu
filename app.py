0af87928e0293698366754d9a2e09306af0f4087
shellradi <meshackmwala8@gmail.com>
2019-09-13 17:51:41 +0300

    added route home and about

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route("/")
+@app.route("/home")
+def home():
+    return "Home Page!"
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
